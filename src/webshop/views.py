122dc8d5d66d19203ae359136ce2b7f120620884
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-15 11:55:34 +0100

    Added assortiment view & product detail view

--- src/webshop/views.py
+++ src/webshop/views.py
@@ -7,3 +7,18 @@ class Index(generic.ListView):
     template_name = 'webshop/index.html'
     model = Product
     context_object_name = 'producten'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['producten'] = Product.objects.all().order_by('?')[:3]
+        return context
+
+class ProductSite(generic.DetailView):
+    template_name = 'webshop/product.html'
+    model = Product
+    context_object_name = 'product'
+
+class Assortiment(generic.ListView):
+    template_name = 'webshop/assortiment.html'
+    model = Product
+    context_object_name = 'producten'
