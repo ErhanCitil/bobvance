eeb13a1c5d473eb7fdb72a5d6644a7be97b56073
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-16 11:48:39 +0100

    Trying to style the contact form

--- src/form/forms.py
+++ src/form/forms.py
@@ -5,3 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+            'order_id': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
