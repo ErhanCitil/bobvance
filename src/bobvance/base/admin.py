6332c9f31d1729769becb7149a7d5269ba6a1f84
Erhan <erhan.citil@maykinmedia.nl>
2023-10-04 16:23:56 +0200

    ♻️  Added list_display to the CustomerAdmin

--- src/bobvance/base/admin.py
+++ src/bobvance/base/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from bobvance.base.models import Customer, Product, OrderProduct, Order
 
 # Register your models here.
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "email")
+admin.site.register(Customer, CustomerAdmin)
+
 admin.site.register(Product)
 
-admin.site.register(OrderProduct)
 class OrderProductInline(admin.TabularInline):
     model = OrderProduct
     extra = 1
 
-@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderProductInline]
+admin.register(Order, OrderAdmin)
