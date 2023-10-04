52c92b516c021c938fe2a63c7e8ccd677ece9bf8
Erhan <erhan.citil@maykinmedia.nl>
2023-10-04 15:15:26 +0200

    ✨ [#1] Added Product, Order and OrderProduct models

--- src/bobvance/base/models.py
+++ src/bobvance/base/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from localflavor.nl.models import NLZipCodeField
 
+from django.core.validators import MinValueValidator
+
 # Create your models here.
 class Customer(models.Model):
     firstname = models.CharField(max_length=50)
@@ -15,3 +17,45 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_img/')
+    new = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+IN_PROGRESS = 'In_progress'
+SHIPPED = 'Shipped'
+DELIVERED = 'Delivered'
+CANCELLED = 'Cancelled'
+
+ORDER_STATUS_CHOICES = (
+    (IN_PROGRESS, 'In progress'),
+    (SHIPPED, 'Shipped'),
+    (DELIVERED, 'Delivered'),
+    (CANCELLED, 'Cancelled'),
+)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_CHOICES[0][0])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer} - {self.status}"
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, related_name='order_products',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_products', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}x"
+
+    class Meta:
+        unique_together = ['order', 'product']
+        ordering = ['order']
