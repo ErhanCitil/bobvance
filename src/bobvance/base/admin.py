e56138c18d728425a22b2fa607e7ee8b91d21880
Erhan <erhan.citil@maykinmedia.nl>
2023-10-04 16:24:58 +0200

    🎨 Registering on the right way

--- src/bobvance/base/admin.py
+++ src/bobvance/base/admin.py
@@ -9,10 +9,11 @@ admin.site.register(Customer, CustomerAdmin)
 
 admin.site.register(Product)
 
+admin.site.register(OrderProduct)
 class OrderProductInline(admin.TabularInline):
     model = OrderProduct
     extra = 1
 
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderProductInline]
-admin.register(Order, OrderAdmin)
