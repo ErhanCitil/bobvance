cebd1e8f898775742c1e5db84af890d41ebc1a9f
Erhan <erhan.citil@maykinmedia.nl>
2023-10-04 16:17:45 +0200

    🎨 Registered all the models in the admin & added OrderProduct as an inline admin model

--- src/bobvance/base/admin.py
+++ src/bobvance/base/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from bobvance.base.models import Customer
+from bobvance.base.models import Customer, Product, OrderProduct, Order
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Product)
+
+admin.site.register(OrderProduct)
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderProductInline]
