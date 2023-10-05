a7b35b1f7c80fb21ce20ae1fb111fae1f23204a3
Erhan <erhan.citil@maykinmedia.nl>
2023-10-05 16:14:56 +0200

    🎨 [#5] Getting random more products

--- src/bobvance/base/views.py
+++ src/bobvance/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from bobvance.base.models import Product
 from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import get_object_or_404
 
 class Home(TemplateView):
     template_name = 'base/index.html'
@@ -17,3 +18,8 @@ class ProductsView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'base/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['more_products'] = Product.objects.exclude(pk=self.object.pk).order_by('?')[:5]
+        return context
