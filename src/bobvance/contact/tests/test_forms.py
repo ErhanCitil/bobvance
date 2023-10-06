a285a0c3f14c4a11e5ce69b6ab3ad1833cc1e5ca
Erhan <erhan.citil@maykinmedia.nl>
2023-10-06 16:08:02 +0200

    ✅ [#9] Added tests for the Contact View

---
+++ src/bobvance/contact/tests/test_forms.py
@@ -0,0 +1,17 @@
+from django_webtest import WebTest
+from django.urls import reverse
+from bobvance.contact.models import Contact
+from bobvance.contact.tests.factories import ContactFactory
+
+class ContactViewTest(WebTest):
+    def setUp(self):
+        self.contact = ContactFactory(
+            name='Bob Vance',
+            email='bobvance@gmail.com',
+            message="I want to buy a"
+        )
+
+    def test_contact_view(self):
+        response = self.app.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/contact.html')
