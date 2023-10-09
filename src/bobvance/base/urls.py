550f5f30550cfa96398168a0c1ed83bd48b3faf7
Erhan <erhan.citil@maykinmedia.nl>
2023-10-09 11:06:41 +0200

    🚧 [#7] Build the logic of the add to cart functionality

--- src/bobvance/base/urls.py
+++ src/bobvance/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from bobvance.base.views import Home, ProductsView, ProductDetailView, CartView
+from bobvance.base.views import Home, ProductsView, ProductDetailView, CartView, AddToCartView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('products/', ProductsView.as_view(), name='products'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('cart/', CartView.as_view(), name='cart'),
+    path('add_to_cart/', AddToCartView.as_view(), name='add_to_cart'),
 ]
