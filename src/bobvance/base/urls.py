141327adebcf1e8bee4dc2323effee7fc0ec3da8
Erhan <erhan.citil@maykinmedia.nl>
2023-10-10 12:37:45 +0200

    🔥 [#19] Removed all JavaScript related code

--- src/bobvance/base/urls.py
+++ src/bobvance/base/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path, include
-from bobvance.base.views import Home, ProductsView, ProductDetailView, CartView, AddToCartView, RemoveFromCartView, UpdateCartView
+from bobvance.base.views import Home, ProductsView, ProductDetailView
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('products/', ProductsView.as_view(), name='products'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('cart/', CartView.as_view(), name='cart'),
-    path('add_to_cart/', AddToCartView.as_view(), name='add_to_cart'),
-    path('remove-from-cart/', RemoveFromCartView.as_view(), name='remove-from-cart'),
-    path('update-cart/', UpdateCartView.as_view(), name='update_cart'),
 ]
